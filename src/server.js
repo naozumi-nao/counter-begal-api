bda540f3dbef65c6ee621694b4e931e69a6c901f
Bima Adityo Kurniawan <dito95.bluestars@gmail.com>
2024-11-25 19:46:15 +0700

    enabled CORS

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 //require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 const mongoose = require('mongoose');
@@ -10,8 +11,7 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Mongo Database'));
 
 app.use(express.json());
-
-
+app.use(cors());
 
 const reportsRouter = require('./routes/reports.js');
 app.use('/reports', reportsRouter);
